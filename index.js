820df6f5dcd09d5ab90976a4babf58ccbb8629db
nattaaek <w,nattaaek@yahoo.com>
2017-04-26 20:32:40 +0700

    askName

--- index.js
+++ index.js
@@ -24,7 +24,23 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs 
 // =========================================================
 // This is called the root dialog. It is the first point of entry for any message the bot receives
-bot.dialog('/', function(session) {
-    // Send 'hello world' to the user
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function(session) {
+        session.beginDialog('/askName');
+    },
+    function(session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
+bot.dialog('/askName', [
+    function(session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function(session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+//osXabmvhMocobHR2MxRTAVa  1431157463615111 62cffb9caed7d9cbc1091f81667644f8
+
+// EAAUVoVA8hocBACsJcYrPG3cxBkWl89vNZCyxp8cHjkD6lIghKeQ2oh0AMyo7BtEmSS99eAztrJ8qsYijw6w72odeJ61RC58TA4vUQuriuDlbeS8NHwOxRJvVS3gqLboHh7rYru0EOj1yYLDsFMQAr1PYZAANePC80ApWXR2QZDZD
